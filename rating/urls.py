00b8c5eb7bb4fae2d14ff43620731912dfe737ce
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 14:48:09 +0300

    set the route and template to display search results

--- rating/urls.py
+++ rating/urls.py
@@ -8,4 +8,5 @@ urlpatterns=[
     url(r'^profile/edit$',views.edit_profile,name='edit'),
     url(r'^project/new$', views.new_project, name='post'),
     url(r'^project/(?P<project_id>[0-9])$',views.project,name='project'),
+    url(r'^search/', views.search,name='search'),
 ]
