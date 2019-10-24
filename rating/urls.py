fb251fe50639e8ef63d7ff179a44ab7ab0484e09
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-25 00:14:28 +0300

    set the routes for the project and profile api calls and direct the app where it can source images

--- rating/urls.py
+++ rating/urls.py
@@ -9,4 +9,11 @@ urlpatterns=[
     url(r'^project/new$', views.new_project, name='post'),
     url(r'^project/(?P<project_id>[0-9])$',views.project,name='project'),
     url(r'^search/', views.search,name='search'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'api/projects/project-id/(?P<pk>[0-9]+)/$', views.ProjectDescription.as_view()),
+    url(r'api/profiles/profile-id/(?P<pk>[0-9]+)/$', views.ProfileDescription.as_view())
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
