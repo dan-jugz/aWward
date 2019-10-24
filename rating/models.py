aac6a55f289a737d09f7993d02574d4e5e2c1f3e
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 11:45:39 +0300

    add a project model to hold all the project detals

--- rating/models.py
+++ rating/models.py
@@ -21,3 +21,22 @@ class technologies(models.Model):
 
     def save_technology(self):
         self.save()
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    landing_page = models.ImageField(upload_to='projects/')
+    description = models.CharField(max_length=255)
+    live_link = models.CharField(max_length=255)
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall = models.IntegerField(blank=True,default=0)
+    categories = models.ManyToManyField(categories)
+    technologies = models.ManyToManyField(technologies)
+    posted  = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
