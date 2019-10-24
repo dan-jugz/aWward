a2256423311bbd016b643d179ded9342d93a4ce2
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 13:27:29 +0300

    fetch the user who is accessing the  various pages

--- rating/views.py
+++ rating/views.py
@@ -17,21 +17,24 @@ def index(request):
 
 @login_required(login_url='/accounts/login')
 def profile(request):
-    
+    current_user=request.user
+    profile =Profile.objects.get(user=current_user)
+    projects = Project.objects.filter(user=current_user)
+    my_profile = Profile.objects.get(user=current_user)
     return render(request, 'profile.html', locals())
 
 
 @login_required(login_url='/accounts/login')
 def edit_profile(request):
-
+    current_user = request.user
     return render(request, 'edit_profile.html', locals())
 
 @login_required(login_url='/accounts/login')
 def new_project(request):
-
+    current_user = request.user
     return render(request, 'new_project.html', locals())
 
 @login_required(login_url='/accounts/login')
 def project(request):
-
+    current_user = request.user
     return render(request, 'project.html', locals())
