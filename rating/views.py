0ccf5fc61985a9ee6ddab304ad8819ff2c1504b6
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 11:55:05 +0300

    write a function that renders the homepage

--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models  import Project
 
 # Create your views here.
+def index(request):
+ 
+    projects = Project.objects.order_by('-overall').all()
+    runners=Project.objects.all()[:4]
+    
+    return render(request, 'index.html', locals())
