914a34122ba4cbce618f666ea40f38baee22abc5
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 23:41:56 +0300

    register the many to many tags as for tech and categories

--- rating/admin.py
+++ rating/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Profile,Project,Rating,technologies,categories
 # Register your models here.
 
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('technologies', 'categories')
+
 admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(Rating)
