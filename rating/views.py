c9695ee64d6ed97557303c478d0bea1bc40a72f8
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 13:19:30 +0300

    import the dependencies required for user to login to access the homepage

--- rating/views.py
+++ rating/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from .models  import Project,Profile
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from .models  import Project,Profile,Rating,categories,technologies
+from .forms import ProfileForm, UploadForm, RatingForm
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
 def index(request):
- 
+    current_user = request.user 
     projects = Project.objects.order_by('-overall').all()
     runners=Project.objects.all()[:4]
     
