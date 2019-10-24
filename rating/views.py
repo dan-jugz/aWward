08c0842a6c4edbcd2b5d60c58b6707a643819c45
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 13:43:02 +0300

    catch the errors that  might occur on rendering the homepage

--- rating/views.py
+++ rating/views.py
@@ -10,8 +10,13 @@ from django.core.exceptions import ObjectDoesNotExist
 def index(request):
     current_user = request.user 
     projects = Project.objects.order_by('-overall').all()
+    top = projects[0]
     runners=Project.objects.all()[:4]
-    
+    try:
+        current_user = request.user
+        profile =Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('edit')
     return render(request, 'index.html', locals())
 
 
