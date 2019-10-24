affda05a16e7ef017cfda7b12fb92a12df2b9fb6
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 12:28:21 +0300

    set the routes associated with the projects as well as the profiles

--- rating/urls.py
+++ rating/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns=[
     url('^$', views.index, name = 'index'),
     url(r'^myprofile$',views.profile,name='myprofile'),
+    url(r'^profile/edit$',views.edit_profile,name='edit'),
+    url(r'^project/new$', views.new_project, name='post'),
+    url(r'^project/(?P<project_id>[0-9])$',views.project,name='project'),
 ]
