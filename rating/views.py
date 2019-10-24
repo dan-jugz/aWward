eee5c0aaea8a5da3ff94f4ce4daf6258918ce737
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-24 13:35:36 +0300

    fetch the profile  details to be edited and save the edited version

--- rating/views.py
+++ rating/views.py
@@ -27,7 +27,16 @@ def profile(request):
 @login_required(login_url='/accounts/login')
 def edit_profile(request):
     current_user = request.user
-    return render(request, 'edit_profile.html', locals())
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = current_user
+            prof.save()
+            return redirect('myprofile')
+    else:
+        form = ProfileForm()
+    return render(request, 'edit_profile.html', {'form': form, 'profile':profile})
 
 @login_required(login_url='/accounts/login')
 def new_project(request):
